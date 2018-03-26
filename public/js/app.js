e52a415427912c2e8ef924ea9af6803fdc586ceb
Javier Avitia <Javier.A.Avitia@gmail.com>
2018-03-25 17:52:57 -0700

    Fixed hyperlink error.

--- public/js/app.js
+++ public/js/app.js
@@ -10,71 +10,78 @@ $(document).on("click", "#btnEvnt", function() {
 	$("#movies").removeClass("active");
 });
 
-$(document).on("click", ".stuff", function(){
-	var station = $(this).find('img').attr('station');
-	var line = $(this).find('img').attr('line');
-	var img = $(this).find('img').attr('src');
-	var type = 'event';
-	var detailsObj = {};
-	var details;
+$(document).on("click", ".stuff", function(e){
+	if (e.target.tagName.toLowerCase() != 'a') {
 
-	detailsObj.venue = $(this).find('span.venue').text();
-	detailsObj.genre = $(this).find('span.genre').text();
-	detailsObj.date = $(this).find('span.date').text();
-	detailsObj.name = $(this).find('span.name').text();
-	detailsObj.distance = $(this).find('span.distance').text();
-	detailsObj.link = $(this).find('a').attr('href');
-	
-	alert("Event saved!");
+		var station = $(this).find('img').attr('station');
+		var line = $(this).find('img').attr('line');
+		var img = $(this).find('img').attr('src');
+		var type = 'event';
+		var detailsObj = {};
+		var details;
 
-	details = JSON.stringify(detailsObj);	
+		detailsObj.venue = $(this).find('span.venue').text();
+		detailsObj.genre = $(this).find('span.genre').text();
+		detailsObj.date = $(this).find('span.date').text();
+		detailsObj.name = $(this).find('span.name').text();
+		detailsObj.distance = $(this).find('span.distance').text();
+		detailsObj.link = $(this).find('a').attr('href');
+		
+		alert("Event saved!");
 
-	
+		details = JSON.stringify(detailsObj);	
 
-	$.post("/api/save", { line, station, img, type, details },
-    	function(data){
-        	console.log(data);
-    	}
-    );
+		
+
+		$.post("/api/save", { line, station, img, type, details },
+	    	function(data){
+	        	console.log(data);
+	    	}
+	    );
+	};
 });
+//can fix, below if works!
+$(document).on("click", ".movies_info", function(e){
+	if (e.target.tagName.toLowerCase() != 'a') {
+		// e.stopPropagation();
 
-$(document).on("click", ".movies_info", function(){
-	var station = $(this).find('img').attr('station');
-	var line = $(this).find('img').attr('line');
-	var img = $(this).find('img').attr('src');
-	var detailsObj = {};
-	var type = 'movie';
+		var station = $(this).find('img').attr('station');
+		var line = $(this).find('img').attr('line');
+		var img = $(this).find('img').attr('src');
+		var detailsObj = {};
+		var type = 'movie';
 
-	detailsObj.rating = $(this).find("h4 .rating").text();
-	detailsObj.title = $(this).find("h4 .title").text();	
-	detailsObj.date = $(this).find('span.date').text();
-	detailsObj.theaters = [];
+		detailsObj.rating = $(this).find("h4 .rating").text();
+		detailsObj.title = $(this).find("h4 .title").text();	
+		detailsObj.date = $(this).find('span.date').text();
+		detailsObj.theaters = [];
 
-	$(this).find(".cineTime").each(function(index,cT){
-		var cineObj = {};
+		$(this).find(".cineTime").each(function(index,cT){
+			var cineObj = {};
 
-		cineObj.name = $(cT).find("h6").text();
-		cineObj.showtimes = [];
+			cineObj.name = $(cT).find("h6").text();
+			cineObj.showtimes = [];
 
-		$(cT).find("a").each(function(index,link){
-			var href = $(link).attr("href");
-			var showtime = $(link).text();			
-			var linkObj = { showtime, href };
+			$(cT).find("a").each(function(index,link){
+				var href = $(link).attr("href");
+				var showtime = $(link).text();			
+				var linkObj = { showtime, href };
 
-			cineObj.showtimes.push(linkObj);
-		})
+				cineObj.showtimes.push(linkObj);
+			})
 
-		detailsObj.theaters.push(cineObj);
+			detailsObj.theaters.push(cineObj);
 
-	});
+		});
 
-	alert("Movie saved!");
+		alert("Movie saved!");
 
-	var details = JSON.stringify(detailsObj);
+		var details = JSON.stringify(detailsObj);
 
-	$.post("/api/save", { line, station, img, type, details },
-    	function(data){
-        	console.log(data);
-    	}
-    );
+		$.post("/api/save", { line, station, img, type, details },
+	    	function(data){
+	        	console.log(data);
+	    	}
+	    );
+	};
 });
